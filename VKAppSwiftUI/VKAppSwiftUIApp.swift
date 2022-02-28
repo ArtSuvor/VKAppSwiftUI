527205fc4b8d482f8976ffe61e01872993d57b55
ArtSuvor <85563543+ArtSuvor@users.noreply.github.com>
2022-02-28 23:00:06 +0300

    Initial Commit

---
+++ VKAppSwiftUI/VKAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKAppSwiftUIApp.swift
+//  VKAppSwiftUI
+//
+//  Created by Art on 28.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

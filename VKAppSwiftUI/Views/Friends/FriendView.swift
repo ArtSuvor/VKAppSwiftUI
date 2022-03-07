9c0f97fbc8c229faf36e063757ea2ba720b4d8f8
ArtSuvor <85563543+ArtSuvor@users.noreply.github.com>
2022-03-07 22:45:32 +0300

    Вью друзей и групп, новые цвета

---
+++ VKAppSwiftUI/Views/Friends/FriendView.swift
@@ -0,0 +1,26 @@
+//
+//  FriendView.swift
+//  VKAppSwiftUI
+//
+//  Created by Art on 07.03.2022.
+//
+
+import SwiftUI
+
+struct FriendView: View {
+    
+    var body: some View {
+        ZStack {
+            Text("asdfasd")
+                .foregroundColor(.white)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.gray)
+    }
+}
+
+struct FriendView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendView()
+    }
+}
---
+++ VKAppSwiftUI/Views/Groups/GroupsView.swift
@@ -0,0 +1,24 @@
+//
+//  GroupsView.swift
+//  VKAppSwiftUI
+//
+//  Created by Art on 07.03.2022.
+//
+
+import SwiftUI
+
+struct GroupsView: View {
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            Color.black
+                .ignoresSafeArea()
+            
+        }
+    }
+}
+
+struct GroupsView_Previews: PreviewProvider {
+    static var previews: some View {
+        GroupsView()
+    }
+}

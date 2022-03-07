b826bb81eab02c93abe01225b29ed2dae2778891
ArtSuvor <85563543+ArtSuvor@users.noreply.github.com>
2022-03-07 18:37:14 +0300

    FriendCell

---
+++ VKAppSwiftUI/Views/Friends/Cell/FriendCell.swift
@@ -0,0 +1,50 @@
+//
+//  SwiftUIView.swift
+//  VKAppSwiftUI
+//
+//  Created by Art on 07.03.2022.
+//
+
+import SwiftUI
+
+struct FriensCell: View {
+    private var isOnline: Bool = false
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.gray)
+            
+            HStack {
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("NameFriend")
+                    Text("Online")
+                        .font(.system(size: 13))
+                        .foregroundColor(isOnline ? .green : .red)
+                }
+                
+                Spacer()
+                
+                Image(systemName: "plus")
+                    .resizable()
+                    .background(
+                        RoundedRectangle(cornerRadius: 50)
+                            .frame(width: 100, height: 100)
+                            .foregroundColor(.white)
+                            .shadow(color: .black, radius: 5)
+                    )
+                    .frame(width: 100, height: 100)
+            }
+            .padding(10)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        .frame(maxWidth: .infinity, maxHeight: 50)
+        .background(Color.clear)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        FriensCell()
+    }
+}
